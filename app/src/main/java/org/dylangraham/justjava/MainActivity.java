cc9b53cc07610d96924b09000e3db2a145bfc081
Dylan Graham <dylan.graham@gmail.com>
2015-12-31 18:05:55 +1100

    Finished

--- app/src/main/java/org/dylangraham/justjava/MainActivity.java
+++ app/src/main/java/org/dylangraham/justjava/MainActivity.java
@@ -68,8 +68,10 @@ public class MainActivity extends ActionBarActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
+        String total;
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        total = "Total: " + NumberFormat.getCurrencyInstance().format(number);
+        priceTextView.setText(total);
     }
 
 }
